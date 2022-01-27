7014946405caf47856b187b20cb57d58b64e5531
Nicolas Tremblay <nick_tremblay456@hotmail.com>
2022-01-27 10:59:50 -0500

    added parent class for CaisseAutomatique and DevineLechiffre

---
+++ c#/PseudoCode/PseudoCode/Class/Devine.cs
@@ -0,0 +1,41 @@
+﻿namespace TNT
+{
+    public class Devine
+    {
+        protected Random random = new Random();
+        protected int randNumber, userGuess;
+        protected bool isConsoleInit = false;
+
+        public virtual void Process()
+        {
+
+        }
+
+        protected void SetConsole()
+        {
+            if (Console.BackgroundColor != ConsoleColor.DarkCyan)
+                Program.SetBackgroundColor(ConsoleColor.DarkCyan);
+            Console.Clear();
+            isConsoleInit = true;
+        }
+
+        protected virtual void GetInput()
+        {
+            try { userGuess = int.Parse(Console.ReadLine()); }
+            catch
+            {
+                Console.WriteLine("La valeur entrée est invalide, entrer un nombre entre 1 et 100");
+                userGuess = 0;
+                return;
+            }
+
+            if (userGuess == randNumber)
+            {
+                Console.WriteLine($"Trouvé! :-) \nLe nombre est: {randNumber}");
+                return;
+            }
+
+            Console.WriteLine(userGuess < randNumber ? "Plus petit, essayer un nombre plus grand" : "Plus grand, essayer un nombre plus petit");
+        }
+    }
+}
