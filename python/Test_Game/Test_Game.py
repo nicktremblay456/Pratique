00422aff760b2f58c6c907b25c45d2d84c42c6be
Nicolas Tremblay <nick_tremblay456@hotmail.com>
2022-01-29 20:12:17 -0500

    added image

--- python/Test_Game/Test_Game.py
+++ python/Test_Game/Test_Game.py
@@ -3,59 +3,68 @@ import random
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-SCREEN_TITLE = "Space Shooter"
-SCALING = 2.0
+SCREEN_TITLE = "Game Test"
 
-class SpaceShooter(arcade.Window):
-    """Space Shooter side scroller game
-    Player starts on the left, enemies appear on the right
-    Player can move anywhere, but not off screen
-    Enemies fly to the left at variable speed
-    Collisions end the game
-    """
+SPRITE_SCALING_COIN = 0.2
+SPRITE_SCALING_PLAYER = 1
+COIN_COUNT = 10
+
+
+class MyGame(arcade.Window):
+    """ Main application class. """
 
     def __init__(self, width, height, title):
-        """Initialize the game
-        """
         super().__init__(width, height, title)
 
-        # Set up the empty sprite lists
-        self.enemies_list = arcade.SpriteList()
-        self.clouds_list = arcade.SpriteList()
-        self.all_sprites = arcade.SpriteList()
+        arcade.set_background_color(arcade.color.AMAZON)
 
-    def setUp(self):
-        """Get the game ready to play
-        """
+    def setup(self):
+        """ Set up the game and initialize the variables. """
 
-        # Set the background color
-        arcade.set_background_color(arcade.color.SKY_BLUE)
+        # Create the sprite lists
+        self.player_list = arcade.SpriteList()
+        self.coin_list = arcade.SpriteList()
+
+        # Score
+        self.score = 0
 
         # Set up the player
-        self.player = arcade.Sprite("images/ship.png", SCALING)
-        self.player.center_y = self.height / 2
-        self.player.left = 10
-        self.all_sprites.append(self.player)
+        # Character image from kenney.nl
+        self.player_sprite = arcade.Sprite("images/circle.jpg", SPRITE_SCALING_PLAYER)
+        self.player_sprite.center_x = 300 # Starting position
+        self.player_sprite.center_y = 300
+        self.player_list.append(self.player_sprite)
 
-        # Spawn a new enemy every 0.25 seconds
-        arcade.schedule(self.add_enemy, 0.25)
+        # Create the coins
+        #for i in range(COIN_COUNT):
+        #
+        #    # Create the coin instance
+        #    # Coin image from kenney.nl
+        #    coin = arcade.Sprite("images/ship.png", SPRITE_SCALING_COIN)
+        #
+        #    # Position the coin
+        #    coin.center_x = random.randrange(SCREEN_WIDTH)
+        #    coin.center_y = random.randrange(SCREEN_HEIGHT)
+        #
+        #    # Add the coin to the lists
+        #    self.coin_list.append(coin)
 
-        # Spawn a new cloud every second
-        arcade.schedule(self.add_cloud, 1.0)
+    def on_draw(self):
+        """ Draw everything """
+        arcade.start_render()
+        self.coin_list.draw()
+        self.player_list.draw()
 
-    def add_enemy(self, delta_time: float):
-        """Adds a new enemy to the screen
+    def update(self, delta_time):
+        """ All the logic to move, and the game logic goes here. """
+        pass
 
-        Arguments:
-            delta_time {float} -- How much time has passed since the last call
-        """
 
-        # First, create the new enemy sprite
-        enemy = arcade.Sprite("images/missile.png", SCALING)
+def main():
+    game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    game.setup()
+    arcade.run()
 
-        # Set its position to a random height and off screen right
-        enemy.left = random.randint(self.width, self.width + 80)
-        enemy.top = random.randint(10, self.height - 10)
 
-game = SpaceShooter(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-game.setUp()
+if __name__ == "__main__":
+    main()
